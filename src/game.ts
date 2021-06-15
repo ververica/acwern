c8c3ddffdc7cdd7783b57c7a14d50229783afe33
Mika Naylor <mail@autophagy.io>
2021-06-15 10:58:12 +0200

    Add basic, actionless multisrc -> operator -> multisink scene

--- src/game.ts
+++ src/game.ts
@@ -1,47 +1,121 @@
-import 'phaser';
+import "phaser";
 
-export default class Demo extends Phaser.Scene
-{
-    constructor ()
-    {
-        super('demo');
+class Source {
+  offset: number;
+  name: string;
+  rate: number;
+  object: Phaser.Physics.Arcade.Image;
+  text: Phaser.GameObjects.Text;
+
+  constructor(offset: number, name: string, rate: number) {
+    this.offset = offset;
+    this.name = name;
+    this.rate = rate;
+  }
+
+  create(scene: Phaser.Scene) {
+    this.object = scene.physics.add.image(50, 75, "tree");
+    this.object.setPosition(50, 75 + (this.object.height + 30) * this.offset);
+    this.text = scene.add.text(
+      25,
+      this.object.height + 10 + (this.object.height + 30) * this.offset,
+      this.name
+    );
+  }
+}
+
+class Operator {
+  name: string;
+  object: Phaser.Physics.Arcade.Image;
+  text: Phaser.GameObjects.Text;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  create(scene: Phaser.Scene) {
+    this.object = scene.physics.add.image(300, 200, "squirrel");
+    this.text = scene.add.text(280, this.object.height / 2 + 200, this.name);
+  }
+}
+
+class Sink {
+  name: string;
+  offset: number;
+  object: Phaser.Physics.Arcade.Image;
+  text: Phaser.GameObjects.Text;
+
+  constructor(offset: number, name: string) {
+    this.offset = offset;
+    this.name = name;
+  }
+
+  create(scene: Phaser.Scene) {
+    this.object = scene.physics.add.image(550, 50, "cave");
+    this.object.setPosition(550, 75 + (this.object.height + 30) * this.offset);
+    this.text = scene.add.text(
+      525,
+      75 + this.object.height / 2 + (this.object.height + 30) * this.offset,
+      this.name
+    );
+  }
+}
+
+export default class Simple extends Phaser.Scene {
+  private sources: Source[];
+  private sinks: Sink[];
+  private operator: Operator;
+
+  constructor() {
+    super("simple");
+
+    let sources = [
+      { name: "src 1", rate: 1 },
+      { name: "src 2", rate: 1 },
+      { name: "src 3", rate: 1 },
+    ];
+    this.sources = sources.map((x, idx) => new Source(idx, x.name, x.rate));
+
+    this.operator = new Operator("map()");
+
+    let sinks = [
+        { name: "snk 1" },
+        { name: "snk 2" }
+    ];
+    this.sinks = sinks.map((x, idx) => new Sink(idx, x.name));
+  }
+
+  preload() {
+    this.load.image("tree", "assets/tree.png");
+    this.load.image("squirrel", "assets/squirrel.png");
+    this.load.image("cave", "assets/cave.png");
+  }
+
+  create() {
+    for (var source of this.sources) {
+      source.create(this);
     }
 
-    preload ()
-    {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
+    this.operator.create(this);
+
+    for (var sink of this.sinks) {
+      sink.create(this);
     }
+  }
 
-    create ()
-    {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
-
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
-
-        this.add.image(400, 300, 'libs');
-
-        const logo = this.add.image(400, 70, 'logo');
-
-        this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
-            repeat: -1
-        })
-    }
+  update() {}
 }
 
 const config = {
-    type: Phaser.AUTO,
-    backgroundColor: '#125555',
-    width: 800,
-    height: 600,
-    scene: Demo
+  type: Phaser.AUTO,
+  backgroundColor: "#125555",
+  width: 800,
+  height: 600,
+  scene: Simple,
+  physics: {
+    default: "arcade",
+    arcade: { debug: true },
+  },
 };
 
 const game = new Phaser.Game(config);
