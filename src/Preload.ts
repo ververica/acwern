e9f645980e3e4de31fef5264a6f631cea2076cd1
Johannes Moser <joe@ververica.com>
2021-11-17 17:53:34 +0100

    added example infrastructure (#6)

--- src/Preload.ts
+++ src/Preload.ts
@@ -12,12 +12,12 @@ export default class Preload extends Phaser.Scene {
         this.load.spritesheet("cave", "assets/cave.png", {frameWidth: 64, frameHeight: 64});
         this.load.spritesheet("acorn", "assets/acorn.png", {frameWidth: 32, frameHeight: 32});
         this.load.image("tiles", "assets/tiles.png");
-        this.load.json("usecase", "examples/99-all.json");
+        this.load.json("usecase", "examples/" + this.registry.get('usecase'))
+        console.log("examples/" + (this.registry.get('usecase') ? this.registry.get('usecase') : "99-all.json"))
     }
     
 
     public create() {
         this.scene.start('acwern')
-        console.log("bla")
     }
 }
