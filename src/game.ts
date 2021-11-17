47d42364458eebc8cbbea1026077ad6deb9af40d
Joe Moser <joe@ververica.com>
2021-11-17 17:51:33 +0100

    added example infrastructure

--- src/game.ts
+++ src/game.ts
@@ -2,6 +2,9 @@ import "phaser"
 import Acwern from "./Acwern"
 import Preload from "./Preload"
 
+let usecase = window.location.hash ? window.location.hash.substring(1) : null
+
+
 const config = {
     type: Phaser.AUTO,
     backgroundColor: "#FFFFFF",
@@ -12,7 +15,26 @@ const config = {
     physics: {
         default: "arcade",
         arcade: { debug: false },
-    },
-};
+    }, data: {
+        usecase: usecase
+    }
+}
 
-const game = new Phaser.Game(config);
+const game = new Phaser.Game(config)
+game.registry.set("usecase", usecase)
+
+let request = new XMLHttpRequest()
+request.onreadystatechange = (event) => {
+    if(request.readyState == 4 && request.status == 200) {
+        let html = ""
+        let data = JSON.parse(request.response)
+        for(let example of data) {
+            html += `<li><a href='index.html#${example.file}'>${example.title}</a></li>`
+        }
+        (document.getElementById('menu') as HTMLElement).innerHTML = html
+    }
+}
+
+request.open("get", "examples/examples.json");
+request.send(null);
+
